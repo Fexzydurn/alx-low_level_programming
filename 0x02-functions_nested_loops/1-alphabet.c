f3e96c87f9230df0c549d34517ccb7224939283f
Fexzydurn <houseoficonsimternational@gmail.com>
2022-09-13 21:02:55 -0700

    My new code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - for alphabet
+ *
+ * description - shows alphabet of letters
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		putchar(i);
+
+	}
+
+	putchar ('\n');
+
+	Return(0);
+
+}
