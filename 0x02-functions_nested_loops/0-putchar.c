9f909894eeb784edc354642378aa90b434656289
Fexzydurn <houseoficonsimternational@gmail.com>
2022-09-13 19:26:38 -0700

    Initial script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - the entry point
+ *
+ * Description - prints _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char word[8] = "_putchar"
+	int i;
+
+	for (i = 0; i <= 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	return (0);
+}
