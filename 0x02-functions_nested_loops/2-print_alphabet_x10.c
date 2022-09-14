6c91b53f82528049aeb3ed5765ec0c84e9316b71
Fexzydurn <houseoficonsimternational@gmail.com>
2022-09-13 22:10:42 -0700

    My alphabet char

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,17 +14,21 @@ void print_alphabet_x10(void)
 
 {
 	int alphabet_count = 0;
+
 	while (alphabet_count < 10)
 
 	{
 		char ch = 'a';
+
 		while (ch <= 'z')
 
 		{
 
 			_putchar(ch);
 			ch++;
+
 		}
+
 		alphabet_count++;
 		_putchar('\n');
 	}
