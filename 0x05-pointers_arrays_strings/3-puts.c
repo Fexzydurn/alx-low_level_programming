8b777999428df4a489012f71c0115f85ff4fe3c4
Fexzydurn <houseoficonsimternational@gmail.com>
2022-09-19 13:41:54 -0700

    My new commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Writes a function that prints a string, followed
+ * by a new line, to stdout.
+ *
+ * @str: an input string
+ *
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
